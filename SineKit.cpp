2e0abc3614a6e40bbe60cfa7979a7b808b4e86d3
Hector van der Aa <h3cx@h3cx.dev>
2025-06-06 23:14:54 +0200

    Initial commit

---
+++ SineKit.cpp
@@ -0,0 +1,8 @@
+#include "SineKit.h"
+
+#include <iostream>
+
+void hello()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ SineKit.h
@@ -0,0 +1,6 @@
+#ifndef SINEKIT_LIBRARY_H
+#define SINEKIT_LIBRARY_H
+
+void hello();
+
+#endif //SINEKIT_LIBRARY_H
