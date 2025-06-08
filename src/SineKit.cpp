a731ae6f83c617467c92c73e8867e76a8888294f
Hector van der Aa <h3cx@h3cx.dev>
2025-06-08 18:54:37 +0200

    Added quasi full WAV parsing and saving, bug with duration during conversion
    TODO: Cleanup

--- src/SineKit.cpp
+++ src/SineKit.cpp
@@ -12,90 +12,247 @@ sk::SineKit::SineKit(AudioType type, unsigned int number_channels) :
                         numChannels(number_channels){}
 
 void sk::SineKit::loadFile(std::filesystem::path input_path) {
-    WAVHeader.readWAVHeader(input_path);
+    std::ifstream file(input_path, std::ios::binary);
+    m_WAVHeader.readWAVHeader(file);
+    numChannels = m_WAVHeader.m_FMTHeader.NumChannels;
+    pcmBitDepth = m_WAVHeader.m_FMTHeader.BitsPerSample;
+    pcmSampleRate = m_WAVHeader.m_FMTHeader.SampleRate;
+    audioType = T_PCM;
+    switch (pcmBitDepth) {
+        case 16: bitType = T_16I; break;
+        case 24: bitType = T_24I; break;
+        case 32: bitType = T_32F; break;
+        case 64: bitType = T_64F; break;
+    }
+
+    numFrames = m_WAVHeader.m_WAVDataHeader.Subchunk2Size / (numChannels*(pcmBitDepth/8));
+
+    pcmBuffer16.clear();
+    pcmBuffer24.clear();
+    pcmBuffer32.clear();
+    pcmBuffer16.clear();
+    pcmBuffer64f.clear();
+
+    switch (bitType) {
+        case T_16I: {
+            std::vector<int16_t> interleaved;
+            interleaved.resize(numFrames*numChannels);
+            for (int i = 0; i < numFrames*2; i++) {
+                int16_t sample;
+                file.read(reinterpret_cast<char*>(&sample), sizeof(sample));
+                interleaved.at(i) = sample;
+            }
+            file.close();
+            pcmBuffer16.resize(numChannels, numFrames);
+            for (int i = 0; i < numFrames; i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    pcmBuffer16.channels.at(j).at(i) = interleaved.at(i*numChannels + j);
+                }
+            }
+            break;
+        }
+        case T_24I: {
+            {
+                std::vector<int32_t> interleaved;
+                interleaved.resize(numFrames * numChannels);
+                for (int i = 0; i < numFrames * numChannels; i++) {
+                    uint8_t bytes[3];
+                    file.read(reinterpret_cast<char*>(bytes), 3);
+
+                    // Convert 3-byte little-endian to signed 32-bit integer
+                    int32_t sample = (static_cast<int32_t>(bytes[2]) << 16) |
+                                     (static_cast<int32_t>(bytes[1]) << 8) |
+                                     (static_cast<int32_t>(bytes[0]));
+                    // Sign-extend if necessary
+                    if (sample & 0x800000) {
+                        sample |= 0xFF000000;
+                    }
+                    interleaved.at(i) = sample;
+                }
+                file.close();
+                pcmBuffer24.resize(numChannels, numFrames);
+                for (int i = 0; i < numFrames; i++) {
+                    for (int j = 0; j < numChannels; j++) {
+                        pcmBuffer24.channels.at(j).at(i) = interleaved.at(i * numChannels + j);
+                    }
+                }
+                break;
+            }
+        }
+        case T_32F : {
+            std::vector<float> interleaved;
+            interleaved.resize(numFrames*numChannels);
+            for (int i = 0; i < numFrames*2; i++) {
+                float sample;
+                file.read(reinterpret_cast<char*>(&sample), sizeof(sample));
+                interleaved.at(i) = sample;
+            }
+            file.close();
+            pcmBuffer32f.resize(numChannels, numFrames);
+            for (int i = 0; i < numFrames; i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    pcmBuffer32f.channels.at(j).at(i) = interleaved.at(i*numChannels + j);
+                }
+            }
+            break;
+        }
+    }
 }
 
-sk::WAVHeader::WAVHeader() : m_RIFFHeader(RIFFHeader()), m_FMTHeader(FMTHeader()), m_WAVDataHeader(WAVDataHeader()) {}
-sk::WAVHeader::~WAVHeader() {
+void sk::SineKit::writeFile(std::filesystem::path output_path) {
+    std::ofstream file(output_path, std::ios::binary);
+    m_WAVHeader.writeWAVHeader(file);
+
+    switch (bitType) {
+        case T_16I: {
+            std::vector<int16_t> interleaved;
+            interleaved.resize(pcmBuffer16.channels.at(0).size() * numChannels);
+            for (int i = 0; i < pcmBuffer16.channels.at(0).size(); i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    interleaved.at(i*numChannels + j) = pcmBuffer16.channels.at(j).at(i);
+                }
+            }
+            for (int i = 0; i < pcmBuffer16.channels.at(0).size()*numChannels; i++) {
+                file.write(reinterpret_cast<char*>(&interleaved.at(i)), 2);
+            }
+            file.close();
+            break;
+        }
+        case T_24I: {
+            std::vector<uint8_t> bytes;
+            bytes.reserve(pcmBuffer24.channels.at(0).size() * numChannels * 3);
+            for (int i = 0; i < pcmBuffer24.channels.at(0).size(); i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    int32_t sample = pcmBuffer24.channels.at(j).at(i);
+                    bytes.push_back(static_cast<uint8_t>(sample & 0xFF));
+                    bytes.push_back(static_cast<uint8_t>((sample >> 8) & 0xFF));
+                    bytes.push_back(static_cast<uint8_t>((sample >> 16) & 0xFF));
+                }
+            }
+            file.write(reinterpret_cast<char*>(bytes.data()), bytes.size());
+            file.close();
+            break;
+        }
+        case T_32F: {
+            std::vector<float> interleaved;
+            interleaved.resize(pcmBuffer32f.channels.at(0).size() * numChannels);
+            for (int i = 0; i < pcmBuffer32f.channels.at(0).size(); i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    interleaved.at(i*numChannels + j) = pcmBuffer32f.channels.at(j).at(i);
+                }
+            }
+            for (int i = 0; i < pcmBuffer32f.channels.at(0).size()*numChannels; i++) {
+                file.write(reinterpret_cast<char*>(&interleaved.at(i)), 4);
+            }
+            file.close();
+        }
+    }
 }
 
-void sk::WAVHeader::readWAVHeader(std::filesystem::path input_path) {
-    m_RIFFHeader.readRIFFHeader(input_path);
-    std::cout << m_RIFFHeader << std::endl;
-    m_FMTHeader.readFMTHeader(input_path);
+void sk::SineKit::convertToFormat(BitType targetBitType) {
+    if (targetBitType == T_24I && bitType == T_16I) {
+        {
+            bitType = T_24I;
+            pcmBitDepth = 24;
+            pcmBuffer24.resize(pcmBuffer16.numChannels, pcmBuffer16.numFrames);
+            for (int i = 0; i < pcmBuffer16.channels.at(0).size(); i++) {
+                for (int j = 0; j < numChannels; j++) {
+                    pcmBuffer24.channels.at(j).at(i) = static_cast<int32_t>(pcmBuffer16.channels.at(j).at(i)) << 8;
+                }
+            }
+            pcmBuffer16.clear();
+        }
+    }
+    if (targetBitType == T_32F && bitType == T_16I) {
+        bitType = T_32F;
+        pcmBitDepth = 32;
+        pcmBuffer32f.resize(pcmBuffer16.numChannels, pcmBuffer16.numFrames);
+        for (int i = 0; i < pcmBuffer16.channels.at(0).size(); i++) {
+            for (int j = 0; j < numChannels; j++) {
+                pcmBuffer32f.channels.at(j).at(i) = static_cast<float>(pcmBuffer16.channels.at(j).at(i)) / 100000.0f;
+            }
+        }
+        pcmBuffer16.clear();
+        pcmBuffer16.clear();
+    }
+    updateHeaders();
+}
+
+void sk::SineKit::updateHeaders() {
+    m_WAVHeader.updateWAVHeader(pcmBitDepth, pcmSampleRate, numChannels, numFrames);
+}
+
+
+
+
+void sk::WAVHeader::readWAVHeader(std::ifstream& file) {
+    m_RIFFHeader.readRIFFHeader(file);
+    std::cout << m_RIFFHeader;
+    m_FMTHeader.readFMTHeader(file);
     std::cout << m_FMTHeader << std::endl;
-    m_WAVDataHeader.readWAVDataHeader(input_path, m_FMTHeader.getSubchunk1Size());
+    m_WAVDataHeader.readWAVDataHeader(file);
     std::cout << m_WAVDataHeader << std::endl;
 }
 
-
-
-
-
-
-
-sk::RIFFHeader::RIFFHeader() : ChunkID(nullptr), ChunkSize(0), Format(nullptr)  {}
-sk::RIFFHeader::~RIFFHeader() {
-    delete[] ChunkID;
-    delete[] Format;
+void sk::WAVHeader::writeWAVHeader(std::ofstream& file) {
+    m_RIFFHeader.writeRIFFHeader(file);
+    m_FMTHeader.writeFMTHeader(file);
+    m_WAVDataHeader.writeWAVDataHeader(file);
 }
 
-void sk::RIFFHeader::readRIFFHeader(std::filesystem::path input_path) {
-    std::fstream file(input_path, std::ios::in | std::ios::binary);
+void sk::WAVHeader::updateWAVHeader(unsigned int bitDepth, unsigned int sampleRate, unsigned int numChannels, unsigned int numFrames) {
+    m_FMTHeader.NumChannels = numChannels;
+    m_FMTHeader.SampleRate = sampleRate;
+    m_FMTHeader.ByteRate = sampleRate * numChannels * bitDepth / 8;
+    m_FMTHeader.BlockAlign = numChannels * bitDepth / 8;
+    m_FMTHeader.BitsPerSample = bitDepth;
 
-    if (!file.is_open()) {
-        std::cerr << "Could not open file " << input_path << std::endl;
-        return;
+    m_WAVDataHeader.Subchunk2Size = numFrames * numChannels * bitDepth / 8;
+
+}
+
+
+
+
+
+// std::ostream& sk::operator<<(std::ostream &os, const RIFFHeader &input) {
+//     os << "Chunk ID: " << input.ChunkID << std::endl;
+//     os << "Chunk size: " << input.ChunkSize << std::endl;
+//     os << "Format: " << input.Format << std::endl;
+//     return os;
+// }
+
+std::ostream& sk::operator<<(std::ostream& os, const Tag& input) {
+    for (size_t i = 0; i < 4; i++) {
+        os << input.v[i];
     }
-
-    ChunkID = new char[5];
-    file.read(ChunkID, 4);
-    ChunkID[4] = '\0';
-
-    file.read(reinterpret_cast<char*>(&ChunkSize), 4);
-
-    Format = new char[5];
-    file.read(Format, 4);
-    Format[4] = '\0';
-    file.close();
+    return os;
 }
 
-uint32_t sk::FMTHeader::getSubchunk1Size() {
-    return Subchunk1Size;
+void sk::RIFFHeader::readRIFFHeader(std::ifstream& file) {
+    file.read(ChunkID.v, sizeof(ChunkID.v));
+    file.read(reinterpret_cast<char*>(&ChunkSize), sizeof(ChunkSize));
+    file.read(Format.v, sizeof(Format.v));
 }
 
+void sk::RIFFHeader::writeRIFFHeader(std::ofstream& file) {
+    file.write(ChunkID.v, sizeof(ChunkID.v));
+    file.write(reinterpret_cast<char*>(&ChunkSize), sizeof(ChunkSize));
+    file.write(Format.v, sizeof(Format.v));
+}
 
-std::ostream& sk::operator<<(std::ostream &os, const RIFFHeader &input) {
+std::ostream& sk::operator<<(std::ostream& os, const RIFFHeader& input) {
     os << "Chunk ID: " << input.ChunkID << std::endl;
-    os << "Chunk size: " << input.ChunkSize << std::endl;
+    os << "Chunk Size: " << input.ChunkSize << std::endl;
     os << "Format: " << input.Format << std::endl;
+    os << std::endl;
     return os;
 }
 
 
+void sk::FMTHeader::readFMTHeader(std::ifstream& file) {
 
-
-
-
-
-sk::FMTHeader::FMTHeader() : Subchunk1ID(nullptr), Subchunk1Size(0) {}
-sk::FMTHeader::~FMTHeader() {
-    delete[] Subchunk1ID;
-}
-
-void sk::FMTHeader::readFMTHeader(std::filesystem::path input_path) {
-    std::fstream file(input_path, std::ios::in | std::ios::binary);
-
-    if (!file.is_open()) {
-        std::cerr << "Could not open file " << input_path << std::endl;
-        return;
-    }
-
-    file.ignore(12);
-
-    Subchunk1ID = new char[5];
-    file.read(Subchunk1ID, 4);
-    Subchunk1ID[4] = '\0';
+    file.read(Subchunk1ID.v, sizeof(Subchunk1ID.v));
 
     file.read(reinterpret_cast<char*>(&Subchunk1Size), sizeof(Subchunk1Size));
 
@@ -121,7 +278,18 @@ void sk::FMTHeader::readFMTHeader(std::filesystem::path input_path) {
 
     file.ignore(OffsetCounter);
 
-    file.close();
+}
+
+void sk::FMTHeader::writeFMTHeader(std::ofstream& file) {
+    file.write(Subchunk1ID.v, sizeof(Subchunk1ID.v));
+    file.write(reinterpret_cast<char*>(&Subchunk1Size), sizeof(Subchunk1Size));
+    file.write(reinterpret_cast<char*>(&AudioFormat), sizeof(AudioFormat));
+    file.write(reinterpret_cast<char*>(&NumChannels), sizeof(NumChannels));
+    file.write(reinterpret_cast<char*>(&SampleRate), sizeof(SampleRate));
+    file.write(reinterpret_cast<char*>(&ByteRate), sizeof(ByteRate));
+    file.write(reinterpret_cast<char*>(&BlockAlign), sizeof(BlockAlign));
+    file.write(reinterpret_cast<char*>(&BitsPerSample), sizeof(BitsPerSample));
+
 }
 
 std::ostream &sk::operator<<(std::ostream &os, const FMTHeader &input) {
@@ -137,32 +305,15 @@ std::ostream &sk::operator<<(std::ostream &os, const FMTHeader &input) {
 }
 
 
-
-
-
-
-sk::WAVDataHeader::WAVDataHeader() : Subchunk2ID(nullptr), Subchunk2Size(0) {}
-sk::WAVDataHeader::~WAVDataHeader() {
-    delete[] Subchunk2ID;
-}
-
-void sk::WAVDataHeader::readWAVDataHeader(std::filesystem::path input_path, uint32_t Subchunk1Size) {
-    std::fstream file(input_path, std::ios::in | std::ios::binary);
-
-    if (!file.is_open()) {
-        std::cerr << "Could not open file " << input_path << std::endl;
-        return;
-    }
-
-    file.ignore(12 + 8 + Subchunk1Size);
-
-    Subchunk2ID = new char[5];
-    file.read(Subchunk2ID, 4);
-    Subchunk2ID[4] = '\0';
+void sk::WAVDataHeader::readWAVDataHeader(std::ifstream& file) {
+    file.read(Subchunk2ID.v, sizeof(Subchunk2ID.v));
 
     file.read(reinterpret_cast<char*>(&Subchunk2Size), sizeof(Subchunk2Size));
+}
 
-    file.close();
+void sk::WAVDataHeader::writeWAVDataHeader(std::ofstream& file) {
+    file.write(Subchunk2ID.v, sizeof(Subchunk2ID.v));
+    file.write(reinterpret_cast<char*>(&Subchunk2Size), sizeof(Subchunk2Size));
 }
 
 std::ostream &sk::operator<<(std::ostream &os, const WAVDataHeader &input) {
--- src/SineKit.h
+++ src/SineKit.h
@@ -1,24 +1,31 @@
 #ifndef SINEKIT_LIBRARY_H
 #define SINEKIT_LIBRARY_H
 
+
+#include <stdlib.h>
 #include <fstream>
 #include <filesystem>
 #include <iostream>
+#include <vector>
 
 namespace sk {
-    class RIFFHeader {
-        char* ChunkID;
-        uint32_t ChunkSize;
-        char* Format;
-    public:
-        RIFFHeader();
-        ~RIFFHeader();
-        void readRIFFHeader(std::filesystem::path input_path);
-        friend std::ostream& operator<<(std::ostream& os, const RIFFHeader& input);
+    struct Tag {
+        char v[4];
     };
+    std::ostream& operator<<(std::ostream& os, const Tag& input);
 
-    class FMTHeader {
-        char* Subchunk1ID;
+    struct RIFFHeader {
+        Tag ChunkID;
+        uint32_t ChunkSize;
+        Tag Format;
+
+        void readRIFFHeader(std::ifstream& file);
+        void writeRIFFHeader(std::ofstream& file);
+    };
+    std::ostream& operator<<(std::ostream& os, const RIFFHeader& input);
+
+    struct FMTHeader {
+        Tag Subchunk1ID;
         uint32_t Subchunk1Size;
         uint16_t AudioFormat;
         uint16_t NumChannels;
@@ -26,32 +33,26 @@ namespace sk {
         uint32_t ByteRate;
         uint16_t BlockAlign;
         uint16_t BitsPerSample;
-    public:
-        FMTHeader();
-        ~FMTHeader();
-        void readFMTHeader(std::filesystem::path input_path);
-        uint32_t getSubchunk1Size();
-        friend std::ostream& operator<<(std::ostream& os, const FMTHeader& input);
+        void readFMTHeader(std::ifstream& file);
+        void writeFMTHeader(std::ofstream& file);
     };
+    std::ostream& operator<<(std::ostream& os, const FMTHeader& input);
 
-    class WAVDataHeader {
-        char* Subchunk2ID;
+    struct WAVDataHeader {
+        Tag Subchunk2ID;
         uint32_t Subchunk2Size;
-    public:
-        WAVDataHeader();
-        ~WAVDataHeader();
-        void readWAVDataHeader(std::filesystem::path input_path, uint32_t Subchunk1Size);
-        friend std::ostream& operator<<(std::ostream& os, const WAVDataHeader& input);
+        void readWAVDataHeader(std::ifstream& file);
+        void writeWAVDataHeader(std::ofstream& file);
     };
+    std::ostream& operator<<(std::ostream& os, const WAVDataHeader& input);
 
-    class WAVHeader {
+    struct WAVHeader {
         RIFFHeader m_RIFFHeader;
         FMTHeader m_FMTHeader;
         WAVDataHeader m_WAVDataHeader;
-    public:
-        WAVHeader();
-        ~WAVHeader();
-        void readWAVHeader(std::filesystem::path input_path);
+        void readWAVHeader(std::ifstream& file);
+        void writeWAVHeader(std::ofstream& file);
+        void updateWAVHeader(unsigned int bitDepth, unsigned int sampleRate, unsigned int numChannels, unsigned int numFrames);
     };
 
     enum AudioType {
@@ -60,21 +61,69 @@ namespace sk {
         T_DSD
     };
 
+    enum BitType {
+        T_16I,
+        T_24I,
+        T_32I,
+        T_32F,
+        T_64F
+    };
+
+    template<typename SampleType>
+    struct AudioBuffer {
+        size_t numFrames;
+        size_t numChannels;
+
+        std::vector<std::vector<SampleType>> channels;
+
+        void resize(size_t numCh, size_t frames) {
+            numFrames = frames;
+            numChannels = numCh;
+
+            channels.resize(numChannels);
+            for (auto &ch : channels)
+                ch.resize(numFrames);
+
+        }
+
+        void clear() {
+            for (auto &ch : channels)
+                ch.clear();
+            channels.clear();
+            numFrames = 0;
+            numChannels = 0;
+        }
+
+        SampleType& sample(size_t ch, size_t frame) {
+            return channels[ch][frame];
+        }
+    };
+
     class SineKit {
-        WAVHeader WAVHeader;
+        WAVHeader m_WAVHeader;
         AudioType audioType;
+        BitType bitType;
         unsigned int pcmBitDepth;
         unsigned int pcmSampleRate;
         unsigned int dsdOversamplingRate;
         unsigned int numChannels;
-        //auto** audioData = nullptr;
+        unsigned int numFrames;
+        AudioBuffer<int16_t> pcmBuffer16;
+        AudioBuffer<int32_t> pcmBuffer24;
+        AudioBuffer<int32_t> pcmBuffer32;
+        AudioBuffer<float> pcmBuffer32f;
+        AudioBuffer<double> pcmBuffer64f;
 
     public:
         SineKit();
         SineKit(AudioType type, unsigned int number_channels);
 
         void loadFile(std::filesystem::path input_path);
+        void writeFile(std::filesystem::path output_path);
+        void convertToFormat(BitType bitType);
+        void updateHeaders();
     };
+
 }
 
 #endif //SINEKIT_LIBRARY_H
