33d7a6a0abd36b5ddc0fb3de853eb039eb2bd425
Hector van der Aa <h3cx@h3cx.dev>
2025-06-10 23:19:17 +0200

    Very broken 80 bit support

---
+++ src/EndianHelpers.h
@@ -0,0 +1,8 @@
+//
+// Created by Hector van der Aa on 6/9/25.
+//
+
+#ifndef ENDIANHELPERS_H
+#define ENDIANHELPERS_H
+
+#endif //ENDIANHELPERS_H
---
+++ src/Extended80.h
@@ -0,0 +1,8 @@
+//
+// Created by Hector van der Aa on 6/10/25.
+//
+
+#ifndef EXTENDED80_H
+#define EXTENDED80_H
+
+#endif //EXTENDED80_H
