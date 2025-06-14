90f64d1ff6425fa1468c7e6b1bfd5dde5976cfe4
Hector van der Aa <h3cx@h3cx.dev>
2025-06-14 23:56:19 +0200

    Began DSF Header structs

---
+++ src/headers/DSFHeaders.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hector van der Aa on 6/14/25.
+//
+
+#include "DSFHeaders.h"
---
+++ src/headers/DSFHeaders.h
@@ -0,0 +1,34 @@
+//
+// Created by Hector van der Aa on 6/14/25.
+//
+
+#ifndef DSFHEADERS_H
+#define DSFHEADERS_H
+
+#include "HeaderTags.h"
+#include <cstdint>
+
+namespace sk::headers::DSF {
+    struct DSDHeader {
+        headers::Tag ChunkID = {{'D','S','D',' '}};
+        std::uint64_t ChunkSize = 28;
+        std::uint64_t FileSize = 0;
+        std::uint64_t MetaPtr = 0;
+    };
+
+    struct FMTHeader {
+        headers::Tag ChunkID = {{'f','m','t',' '}};
+        std::uint64_t ChunkSize = 52;
+        std::uint32_t Format = 1;
+        std::uint32_t FormatID = 0;
+        std::uint32_t ChanType = 2;
+        std::uint32_t ChanNum = 2;
+        std::uint32_t SampleRate = 0;
+        std::uint32_t BitsPerSample = 1;
+        std::uint64_t NumSamples = 0;
+        std::uint32_t BlockSize = 4096;
+        std::uint32_t Reserved = 0;
+    };
+}
+
+#endif //DSFHEADERS_H
